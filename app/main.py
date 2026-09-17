aaa1d1c826ee3c9b1d9d6201bc69afbd660d9af1
kunal1415 <kunalwalunj2006@gmail.com>
2026-09-17 19:23:38 +0530

    feat: add FastAPI request and response schemas

---
+++ app/main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from app.schemas import EmailRequest, PredictionResponse
+from src.prediction import predict_spam
+
+
+app = FastAPI(
+    title="Email Spam Classifier API",
+    description="API for classifying emails as SPAM or HAM using NLP and Machine Learning",
+    version="1.0.0"
+)
+
+
+@app.get("/")
+def home():
+    return {
+        "message": "Email Spam Classifier API is running"
+    }
+
+
+@app.post(
+    "/predict",
+    response_model=PredictionResponse
+)
+def predict_email(request: EmailRequest):
+
+    prediction = predict_spam(request.email)
+
+    return PredictionResponse(
+        prediction=prediction
+    )
